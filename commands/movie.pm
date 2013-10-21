9d355633e0ca366054f8ec35c139bfd615571362
odchbot <odchbot@gmail.com>
2013-10-21 21:34:26 +1100

    Little bit of a tidy-up and rework of the movie command. Primarily, took out vote counts and put a limit on the while loop. Fixes #36

--- commands/movie.pm
+++ commands/movie.pm
@@ -19,47 +19,44 @@ my $api = new WWW::TheMovieDB({
 sub main {
   my $command = shift;
   my $user = shift;
-  my $message = '';
-  my ($name, $title, $rtng, $desc, $year) = '';
-  
-  while(1) {
+  my $message = 'No movie found try again though~ :3';
+  my $i = 0;
+
+  do {{
     my $random_number = int(rand(10000));
     my $info = $api->Movies::info({'movie_id' => $random_number});
     my $json = decode_json($info);
-    if ($json->{'status_code'}) { next; }
-		 
-      my $adult = $json->{'adult'};
-      if ($adult ne 'false')  { next;}
-	  my $languages = $json->{'spoken_languages'};
-      my $foreign = 1;
-      foreach my $language (@$languages) {
-      if ($language->{'iso_639_1'} eq 'en') { $foreign = 0;}}
-	  unless ($foreign == 0) { next;}
-	  my $status = $json->{'status'};
-	  if (($status) eq 'In Production') { next; }
-	  my $vote = $json->{'vote_count'};
-	  if ($vote < 100) { next; }
-	  my $rtng = $json->{'vote_average'};
-      if ($rtng < 6.0) { next; };
-	  my $name = $json->{'original_title'};
-      my $desc = $json->{'overview'};
-	  my $full_date = $json->{'release_date'};
-      my ($year) = split( /\-/, $full_date );
-	  if ($year < 1960) { next; } 
-      return ($name,$rtng,$desc,$year);
-	  }
-      
-    
+    next if ($json->{'status_code'}
+      || $json->{'adult'} ne 'false'
+      || $json->{'status'} eq 'In Production'
+      || 1960 > substr($json->{'release_date'}, 0, 4)
+    );
+    my $languages = $json->{'spoken_languages'};
+    my $foreign = 1;
+    foreach my $language (@$languages) {
+      if ($language->{'iso_639_1'} eq 'en') {
+        $foreign = 0;
+      }
+    }
+    next unless ($foreign == 0);
 
- 
+    $message = "I found you a movie!";
+    $message .= "\nTitle => " . $json->{'original_title'};
+    $message .= "\nYear => " . substr($json->{'release_date'}, 0, 4);
+    $message .= "\nGenres => ";
+    my $genres = $json->{'genres'};
+    foreach my $genre (@$genres) {
+      $message .= "$genre->{'name'}, ";
+    }
+    $message .= "\nRating => " . $json->{'vote_average'};
+    $message .= "\nDescription => " . $json->{'overview'};
+    last;
+  }} while ($i++ < 5);
 
-    my @return = ();
-	
-	@return = (
-	  
+  my @return = (
     {
       param => "message",
-      message => sprintf("Title=> %s\nYear=> %s\nRating=> %s\nDescription=> %s\n",$name,$year,$rtng,$desc),
+      message => $message,
       user => $user->{name},
       touser => '',
       type => 2,
@@ -71,4 +68,3 @@ sub main {
  }
 
 1;
-
