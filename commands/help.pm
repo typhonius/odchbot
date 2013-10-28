40d1c782c7303fc72ed17dcaf3a7162fcea113cb
Wickfish <yacolt@gmail.com>
2013-10-28 19:01:25 +1100

    Update help.pm
    Edited by Wickfish

--- commands/help.pm
+++ commands/help.pm
@@ -1,27 +1,40 @@
 package help;
-
+ 
 use strict;
 use warnings;
 use FindBin;
-use lib "$FindBin::Bin/..";
 use DCBCommon;
-
+use lib "$FindBin::Bin/..";
+ 
 sub main {
   my $command = shift;
   my $user = shift;
-
+ 
   my @return = ();
-
+  my $message = "";
+  my $hub_name = DCBSettings::config_get('hubname');
+  my $hub_href = DCBSettings::config_get('website');
+ 
+  $message = "Welcome to " . $hub_name . " Displaying help and tips!\n";
+  $message .= "If you are having connection issues, try the following steps in order:\n";
+  $message .= "1. Make sure you are running the client as Administrator.\n";
+  $message .= "2. Make sure you are not connecting through a wireless connection or using a router.\n";
+  $message .= "3. Refresh your IP address and restart the client.\n";
+  $message .= "4. In your connection settings, make sure you are using \'Direct connection\'.\n";
+  $message .= "5. Restart your your client and try to connect again and check to see if the problem has been fixed.\n";
+  $message .= "Detailed guides are for help with these steps are available on the website, " . $hub_href . "/\n";
+  $message .= "If you are still having issues, please contact us through chat or email and make sure to include ANY messages ChaosBot is sending you, in full.\n";
+ 
   @return = (
     {
       param    => "message",
-      message  => "Placeholder for help command",
-      user     => '',
+      message  => "$message",
+      user     => $user->{'name'},
       touser   => '',
-      type     => MESSAGE->{'PUBLIC_ALL'},
+      type     => MESSAGE->{PUBLIC_SINGLE},
     },
   );
   return @return;
 }
-
+ 
 1;
