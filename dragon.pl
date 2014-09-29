f38ab5716c1947bb474ecf4e95d97eb67a8f026c
Adam Malone <adam@adammalone.net>
2014-09-29 15:50:30 +1000

    Added more to the dragon script to make it work with the backend

--- dragon.pl
+++ dragon.pl
@@ -3,16 +3,15 @@
 use strict;
 use warnings;
 
-#use Getopt::Euclid; # Uses POD at bottom of file to auto-parse ARGV.
 #use Log::Log4perl qw(:easy); # Also gives additional debug info back from bot.
 use Net::Jabber::Bot;
 use Data::Dumper;
 
 use DCBSettings;
 use DCBDatabase;
-#use DCBCommon;
-#use DCBUser;
-#use Data::Dumper;
+use DCBCommon;
+use DCBUser;
+use Data::Dumper;
 
 eval {
   our $Settings = new DCBSettings;
@@ -20,7 +19,21 @@ eval {
 
   our $Database = new DCBDatabase;
   $Database->db_init();
+
+  our $Common = new DCBCommon;
+  $Common->common_init();
+  $Common->commands_init();
+
+  our $User = new DCBUser;
+  $User->user_init('');
+
+  # odch_hooks('init');
 };
+if ($@) {
+  #jabber_sendmessage("","",4,"$@");
+  #jabber_debug("init","","$@");
+  #die;
+}
 
 # Init log4perl based on command line options
 my %log4perl_init;
@@ -31,33 +44,26 @@ $log4perl_init{'email'} = $ARGV{'-email'} if(defined $ARGV{'-email'});
 $log4perl_init{'debug_level'} = $ARGV{'-debuglevel'};
 #InitLog4Perl(%log4perl_init);
 
-my @forums = ('apj', 'support', 'ops', 'pso');
-
 my $jabber  = $DCBSettings::config->{jabber};
-my $username = $jabber->{username};
-my $password = $jabber->{password};
-my $server = $jabber->{server};
-my $conference_server = $jabber->{conference_server};
-my $port = $jabber->{port};
 
 my %alerts_sent_hash;
 my %next_alert_time_hash;
 my %next_alert_increment;
 
 my %forums_and_responses;
-foreach my $forum (@forums) {
-    my $responses = "bot:|lol hai|"; # Note the pipe at the end indicates it will act on all messages
-    my @response_array = split(/\|/, $responses);
-    push @response_array, "" if($responses =~ m/\|\s*$/);
-    $forums_and_responses{$forum} = \@response_array;
+foreach my $forum (@{ $jabber->{forums} }) {
+  my $responses = "bot:|lol hai|"; # Note the pipe at the end indicates it will act on all messages
+  my @response_array = split(/\|/, $responses);
+  push @response_array, "" if($responses =~ m/\|\s*$/);
+  $forums_and_responses{$forum} = \@response_array;
 }
 
 my $bot = Net::Jabber::Bot->new(
-  server => $server,
-  conference_server => $conference_server,
-  port => $port,
-  username => $username,
-  password => $password,
+  server => $jabber->{server},
+  conference_server => $jabber->{conference_server},
+  port => $jabber->{port},
+  username => $jabber->{username},
+  password => $jabber->{password},
   safety_mode => 1,
   message_function => \&new_bot_message,
   background_function => \&background_checks,
@@ -68,12 +74,14 @@ my $bot = Net::Jabber::Bot->new(
 );
 
 
-foreach my $forum (@forums) {
+foreach my $forum (@{ $jabber->{forums} }) {
 #    $bot->SendGroupMessage($forum, "$alias logged into $forum");
 }
 
 sub background_checks {
-
+# bot_object
+#ROSTERDB->JIDS
+#PRESENCEDB
 }
 
 sub new_bot_message {
@@ -84,7 +92,7 @@ sub new_bot_message {
     if ($bot_message_hash{body} =~ /^(.*\s)?apac(\s.*)?$/i && $bot_message_hash{reply_to} =~ 'apj@conference.acquia.com') {
       $bot->SendGroupMessage($bot_message_hash{reply_to}, "The preferred name is APJ thank you please.");
     }
-    if (($bot_message_hash{body} =~ /^(.*\s)?apj(\s.*)?$/i || $bot_message_hash{body} =~ /^(.*\s)?apac(\s.*)?$/i) && $bot_message_hash{reply_to} =~ 'support@conference.acquia.com') {
+    if (($bot_message_hash{body} =~ /^(.*[\s\W])?apac([\s\W].*)?$/im || $bot_message_hash{body} =~ /^(.*[\s\W])?apj([\s\W].*)?$/im) && $bot_message_hash{reply_to} =~ 'support@conference.acquia.com') {
       $bot->SendGroupMessage($bot_message_hash{reply_to}, "APJ == Best Region");
     }
   }
